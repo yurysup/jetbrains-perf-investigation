6b9a6e08da09068d71993171753024094b56f3a0
Yury Suponeu <usuponev@gmail.com>
2023-11-05 20:42:14 +0400

    benchmark multiple binaries at once

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,8 @@ import time
 import re
 import csv
 import json
+import os
+import signal
 import numpy as np
 from functools import wraps
 
@@ -40,27 +42,34 @@ def write_csv_report(aggregated_data, filename="reports/report.csv"):
             writer.writerow(data_dict)
 
 
-def timing_decorator(function):
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = function(*args, **kwargs)
-        end_time = time.perf_counter()
-        elapsed_time = end_time - start_time
-        return result, elapsed_time
-
-    return wrapper
-
-
-@timing_decorator
-def exec_process(binary: str, max_prime: int, timeout: int) -> bytes:
-    result = subprocess.check_output(
-        f"gtime -v ./bin/{binary} {max_prime} > /dev/null",  # Redirect only ./main output to /dev/null
-        stderr=subprocess.STDOUT,
-        shell=True,
-        timeout=timeout,
+def exec_process(binary: str, max_prime: int):
+    return subprocess.Popen(
+        ["gtime", "-v", f"./bin/{binary}", str(max_prime)],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.PIPE,
+        preexec_fn=os.setsid,
     )
-    return result
+
+
+def check_binaries(binaries: list):
+    # Check if all the required binaries exist in the specified bin directory.
+    # Check if bin_dir is actually a directory
+    bin_dir = "./bin"
+    if not os.path.isdir(bin_dir):
+        print(f"The directory {bin_dir} does not exist.")
+        return False
+
+    missing_binaries = []
+    # Iterate over the list of binaries and check each one
+    for binary in binaries:
+        binary_path = os.path.join(bin_dir, binary)
+        if not os.path.isfile(binary_path):
+            missing_binaries.append(binary)
+
+    if missing_binaries:
+        print(f"The following binaries are missing: {', '.join(missing_binaries)}")
+        return False
+    return True
 
 
 # Extract values from the configuration
@@ -73,84 +82,91 @@ timeout_warmup = config["warmup"]["timeout_seconds"]
 binaries = config["binaries"]
 
 
-aggregated_data = []
+if not check_binaries(binaries=binaries):
+    exit(-1)
 
+
+aggregated_data = []
 for binary in binaries:
+    failed = False
     for max_prime in max_primes:
-        failed = False
+        if failed:
+            break
         time_measurements = []
         memory_measurements = []
         cpu_measurements = []
 
         for _ in range(iterations_warmup):
             try:
-                result_bytes, exec_time = exec_process(
-                    binary=binary, max_prime=max_prime, timeout=timeout_warmup
-                )
-                result = result_bytes.decode("utf-8")
+                process = exec_process(binary=binary, max_prime=max_prime)
+                process.communicate(timeout=timeout_warmup)
             except subprocess.TimeoutExpired:
                 failed = True
+                # Send the SIGTERM signal to the process group to terminate all processes in the group
+                os.killpg(os.getpgid(process.pid), signal.SIGINT)
+                time.sleep(1)
+                process.communicate()
                 print(
-                    f"Timeout reached for {binary} with maxPrime = {max_prime}. Skipping..."
+                    f"Timeout (warmup) reached for {binary} with maxPrime = {max_prime}. Skipping..."
                 )
                 break
-            except subprocess.CalledProcessError as e:
+            except FileNotFoundError as e:
                 failed = True
                 print(e)
                 print(
-                    f"Something went wrong while executing subprocess {binary}: check gtime installation or the compiled executable."
+                    f"Something went wrong while executing subprocess {binary}: check gtime installation."
                 )
                 break
 
         if not failed:
             for _ in range(iterations):
                 try:
-                    result_bytes, exec_time = exec_process(
-                        binary=binary, max_prime=max_prime, timeout=timeout
-                    )
-                    result = result_bytes.decode("utf-8")
+                    start_time = time.perf_counter()
+                    process = exec_process(binary=binary, max_prime=max_prime)
+                    stdout_data, stderr_data = process.communicate(timeout=timeout)
+                    exec_time = time.perf_counter() - start_time
+                    result = stderr_data.decode("utf-8")
                     measurements = get_gtime_measurements(result)
                     time_measurements.append(exec_time)
                     memory_measurements.append(measurements["memory"])
                     cpu_measurements.append(measurements["cpu_pct"])
                 except subprocess.TimeoutExpired:
                     failed = True
+                    # Send the SIGTERM signal to the process group to terminate all processes in the group
+                    os.killpg(os.getpgid(process.pid), signal.SIGINT)
+                    time.sleep(1)
+                    process.communicate()
                     print(
                         f"Timeout reached for {binary} with maxPrime = {max_prime}. Skipping..."
                     )
                     break
-                except subprocess.CalledProcessError as e:
+                except FileNotFoundError as e:
                     failed = True
                     print(e)
                     print(
-                        f"Something went wrong while executing subprocess {binary}: check gtime installation or the compiled executable."
+                        f"Something went wrong while executing subprocess {binary}: check gtime installation."
                     )
                     break
 
         if not failed:
-            """
-            average_time = total_time / iterations
-            average_memory = total_memory / iterations
-            average_cpu_pct = total_cpu_pct / iterations
-            aggregated_data.append(
-                {
-                    'test_label': binaries,
-                    'max_prime': max_prime,
-                    'average_execution_time': average_time,
-                    'average_memory': average_memory,
-                    'average_cpu_pct': average_cpu_pct,
-                }
-            )
-            """
             std_dev_time = np.std(time_measurements)
             mean_time = np.mean(time_measurements)
             mean_memory = np.mean(memory_measurements)
             mean_cpu = np.mean(cpu_measurements)
+            aggregated_data.append(
+                {
+                    "test_label": binary,
+                    "max_prime": max_prime,
+                    "average_execution_time": mean_time,
+                    "average_memory": mean_memory,
+                    "average_cpu_pct": mean_cpu,
+                }
+            )
             print(f"For {binary} with maxPrime = {max_prime}:")
-            print(f"    Average execution time: {mean_time:.4f}s")
-            print(f"    Average memory used: {mean_memory:.2f} kbytes")
-            print(f"    Average CPU used: {mean_cpu:.1f} %")
-            print(f"    Time measurement deviation: {std_dev_time}")
+            print(
+                f"    Average execution time: {mean_time:.4f}s (+/-{std_dev_time:.4f}s)"
+            )
+            print(f"    Average memory used: {mean_memory:.0f} kbytes")
+            print(f"    Average CPU used: {mean_cpu:.0f} %")
 
-# print(aggregated_data)
-# write_csv_report(aggregated_data)
+write_csv_report(aggregated_data)
